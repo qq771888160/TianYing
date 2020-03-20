28fe52a9da683a01ede603b7d72f55f0a01ed7c1
huweijian <771888160@qq.com>
2020-03-20 14:46:55 +0800

    add companyaddlist

---
+++ TianYing/Controllers/CompanyCollectionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TianYing.Core.Interfaces;
+using TianYing.Infrasturcture.Resources;
+
+namespace TianYing.Controllers
+{
+    [ApiController]
+    [Route("api/companycollections")]
+    public class CompanyCollectionsController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly ICompanyRepository companyRepository;
+
+        public CompanyCollectionsController(IMapper mapper, ICompanyRepository companyRepository)
+        {
+            this.mapper = mapper;
+            this.companyRepository = companyRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<CompanyResource>>> CreateCompanyCollection()
+        {
+
+        }
+    }
+}
